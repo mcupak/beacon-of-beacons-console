7b8886e50c8c56d4929ae7bd25f7d0b921ed59a8
Santina Lin <santina424@gmail.com>
2015-06-09 08:23:24 -0700

    Added two way data binding to widget generation
    - Refactored code in `beacon_widget.js`
    - Created `api.js` for query beacons and responses
    - Added additional parameters for widget generation

--- src/js/controllers/beacon_widget.js
+++ src/js/controllers/beacon_widget.js
@@ -1,84 +1,87 @@
 /**
  * Beacon Controller
  */
-
+ 
 angular.module('RDash')
 .controller('BeaconWidgetCtrl', ['$scope', 'API', function($scope, API) {
-
-    API.getBeacons()
-    .success(function(data) {
-        console.log(data);
-        for (key in data) {
-            for (ds in data[key]) {
-                data[key][ds].checked = false;
+ 
+    function groupResponseByOrganization(response) {
+        var groupedResponse = {};
+        for (var i = 0; i < response.length; i++) {
+            var current = response[i];
+            if (groupedResponse[current.organization] === undefined) {
+                var object = [{"name": current.name, "aggregator": current.aggregator, "id":current.id}];
+                groupedResponse[current.organization] = object;
+            }
+            else {
+                groupedResponse[current.organization].push({"name": current.name, "aggregator": current.aggregator, "id":current.id});
             }
         }
-
-        $scope.organizations = data;
+        return groupedResponse;
+    }
+ 
+    API.getBeacons()
+    .success(function(data) {
+        var organizations = groupResponseByOrganization(data);
+        for (key in organizations) {
+            for (ds in organizations[key]) {
+                organizations[key][ds].checked = false;
+            }
+        }
+ 
+        $scope.organizations = organizations;
     })
     .error(function(error) {
         console.log('Failed: ' + error);
         $scope.error = error;
         console.log($scope.error);
     });
-
-
-    $scope.chromosomes = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","X","Y","MT"];
-
-
-    //search parameter: 
+ 
+ 
+    $scope.chromosomes = ["1","2","3","4","5","6","7","8","9","10","11","12","13",
+                          "14","15","16","17","18","19","20","21","22","X","Y","MT"];
+ 
+    // Search parameters:
     $scope.chr = "1";
     $scope.allele = "A";
     $scope.ref = "hg18";
-    $scope.pos = "";
-
+    $scope.pos = 0;
+ 
     $scope.selectAll = function(selected) {
-
         for (var key in $scope.organizations) {
             var org = $scope.organizations[key];
-
             for (var key2 in org) {
                 var dataset = org[key2];
-
                 dataset.checked = selected;
             }
         }
     }
-
-    $scope.refreshWidgetCode = function() {
-
-        console.log("Refreshing code");
-
+ 
+    $scope.code = function() {
+        var organizations = $scope.organizations;
         var ids = [];
-
-        for (var key in $scope.organizations) {
-            var org = $scope.organizations[key];
-
-            for (var key2 in org) {
-                var dataset = org[key2];
-
-                if (dataset.checked) {
-                    ids.push("'" + dataset.id + "'");
+ 
+        for (var key in organizations) {
+            var organization = organizations[key];
+            for (var beaconIndex in organization) {
+                var beacon = organization[beaconIndex];
+                if (beacon.checked) {
+                    ids.push("\"" + beacon.id + "\"");
                 }
             }
         }
-
-
-
-        $scope.code =
-            "<script src=\"http://dnastack.com/ga4gh/bob/js/bobby.js\"></script>\n" +
-            "<script>(\n" +
-            "    function() {\n" +
-            "        var beacons =[" + ids.join() + "];\n\n" +
-            "        /* TODO: set these variables as per: https://github.com/ga4gh/schemas/blob/master/src/main/resources/avro/beacon.avdl */\n" +
-            "        var chromosome=1;\n" +
-            "        var allele=\"A\";\n" +
-            "        var position=1;\n" +
-            "        var genome=\"hg19\";\n\n" +
-            "        writeIFrame(chromosome, position, allele, genome, beacons);\n" +
-            "    })();\n" +
-            "</script>";
-
+ 
+        return "<script src=\"http://dnastack.com/ga4gh/bob/js/bobby.js\"></script>\n" +
+        "<script>(\n" +
+        "    function() {\n" +
+        "        var beacons = [" + ids + "];\n" +
+        "        var chromosome = " + $scope.chr + ";\n" +
+        "        var allele = \"" + $scope.allele + "\";\n" +
+        "        var position = " + $scope.pos + ";\n" +
+        "        var genome = \"" + $scope.ref + "\";\n\n" +
+        "        writeIFrame(chromosome, position, allele, genome, beacons);\n" +
+        "    })();\n" +
+        "</script>";
     }
-
+ 
 }]);
--- src/js/services/api.js
+++ src/js/services/api.js
@@ -1,6 +1,6 @@
-angular.module("RDash").factory("API", function($http) {
-	var API_ENDPOINT = "http://dnastack.com/bob-api/rest"; 
-
+angular.module("RDash").factory("API", ['$http', function($http) {
+	var API_ENDPOINT = "http://dnastack.com/bob-api/rest";
+ 
 	return {
 		getBeacons: function() {
 			return $http.get(API_ENDPOINT + "/beacons");
@@ -9,4 +9,4 @@ angular.module("RDash").factory("API", function($http) {
 			return $http.get(API_ENDPOINT + "/responses" + searchString);
 		}
 	};
-});
+}]);
