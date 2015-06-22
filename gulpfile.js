a7e9f2acb6126bbeacc1c2d54380ad9b497f395f
Santina Lin <santina424@gmail.com>
2015-06-22 10:44:59 -0700

    Edited tracking of css stylesheets

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var gulp = require('gulp'),
 
 var paths = {
     scripts: 'src/js/**/*.*',
-    styles: 'src/less/**/*.*',
+    styles: 'src/css/**/*.*',
     images: 'src/img/**/*.*',
     templates: 'src/templates/**/*.html',
     index: 'src/index.html',
@@ -48,7 +48,7 @@ gulp.task('copy-bower_fonts', function() {
 /**
  * Handle custom files
  */
-gulp.task('build-custom', ['custom-images', 'custom-js', 'custom-less', 'custom-templates']);
+gulp.task('build-custom', ['custom-images', 'custom-js', 'custom-templates']);
 
 gulp.task('custom-images', function() {
     return gulp.src(paths.images)
@@ -61,11 +61,6 @@ gulp.task('custom-js', function() {
         .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('custom-less', function() {
-    return gulp.src(paths.styles)
-        .pipe(less())
-        .pipe(gulp.dest('dist/css'));
-});
 
 gulp.task('custom-templates', function() {
     return gulp.src(paths.templates)
@@ -78,7 +73,7 @@ gulp.task('custom-templates', function() {
  */
 gulp.task('watch', function() {
     gulp.watch([paths.images], ['custom-images']);
-    gulp.watch([paths.styles], ['custom-less']);
+    gulp.watch([paths.styles], ['usemin']);
     gulp.watch([paths.scripts], ['custom-js']);
     gulp.watch([paths.templates], ['custom-templates']);
     gulp.watch([paths.index], ['usemin']);
