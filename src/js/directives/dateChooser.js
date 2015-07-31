78fd46c602ec2f3e1c3047368a4ec480187993e2
Santina Lin <santina424@gmail.com>
2015-08-01 00:34:52 +0900

    Input control and additional attributes
    - Refactor code to use ng-click to open calendars
    - Added optional attributes for min and max date
    - Input control to ensure valid date range

--- src/js/directives/dateChooser.js
+++ src/js/directives/dateChooser.js
@@ -2,7 +2,12 @@
     A date range directive
     that wraps around two popup Angular UI Bootstrap datepickers
     to allow selection of two different dates. 
-    Usage example: <daterange startdate="date1" enddate="date2"></daterange> 
+    Usage example: <daterange startdate="date1" enddate="date2" mindate="lastFriday" maxdate="today"></daterange>
+
+    `mindate` and `maxdate` are optional and they take in a Date object. For example, 
+    `maxdate` could be set it to today's day by etting the attribute to "today" and 
+    declaring ' $scope.today = new Date(); ' 
+    in the view's controller
 */ 
 var app = angular.module('RDash'); 
 app.directive('daterange', function () {
@@ -10,52 +15,68 @@ app.directive('daterange', function () {
         restrict: 'E',
         replace: true,
         scope: {
-            startdate: '=',
-            enddate: "="
+            startdate: '=startdate',
+            enddate: "=enddate", 
+            mindate: "=mindate",
+            maxdate: "=maxdate"
         },
         template: 
             '<div class="date-range-selector">' + 
             '<div class="date-start col-lg-6">' +
-                '<div>Start Date: </div>' + 
+                '<div style="float:left; padding:10px">From </div>' + 
                 '<div class="input-group">' +
-                  '<input ng-model="startdate" type="text" class="form-control" datepicker-popup="yyyy-MM-dd" is-open="opened" datepicker-options="dateOptions" ng-required="true" close-text="Close">' +
+                  '<input ng-model="startdate" type="text" class="form-control" datepicker-popup="yyyy-MM-dd" is-open="opened" min-date="mindate" max-date="maxdate" datepicker-options="dateOptions" ng-required="true" close-text="Close">' +
                   '<span class="input-group-btn">' +
-                    '<button type="button" class="buttonstart btn btn-default"><i class="glyphicon glyphicon-calendar"></i></button>' +
+                    '<button ng-click="openStartCalendar()" type="button" class="buttonstart btn btn-default"><i class="glyphicon glyphicon-calendar"></i></button>' +
                   '</span>' +
                 '</div>' + 
             '</div>' + 
             '<div class="date-end col-lg-6">' +
-                '<div>End Date: </div>' + 
+                '<div style="float:left; padding:10px">To </div>' + 
                 '<div class="input-group">' +
-                  '<input ng-model="enddate" type="text" class="form-control" datepicker-popup="yyyy-MM-dd" is-open="opened2" datepicker-options="dateOptions" ng-required="true" close-text="Close">' +
+                  '<input ng-model="enddate" type="text" class="form-control" datepicker-popup="yyyy-MM-dd" is-open="opened2" min-date="minEndDate" max-date="maxdate" datepicker-options="dateOptions" ng-required="true" close-text="Close">' +
                   '<span class="input-group-btn">' +
-                    '<button type="button" class="buttonend btn btn-default"><i class="glyphicon glyphicon-calendar"></i></button>' +
+                    '<button ng-click="openEndCalendar()" type="button" class="buttonend btn btn-default"><i class="glyphicon glyphicon-calendar"></i></button>' +
                   '</span>' +
                 '</div>' + 
             '</div>' + 
             '</div>',
             
-        controller: function ($scope) {
+        controller: function ($scope, $element, $attrs) {
             $scope.opened = false;
             $scope.opened2 = false;
+            $scope.minEndDate = $scope.mindate; 
         },
-        link: function (scope, element) {
+        link: function (scope, element, attrs) {
             scope.dateOptions = {
                 formatYear: 'yy',
                 startingDay: 1
             };
-            element.find('.buttonstart').bind('click', function (event) {
+
+            // Open the first calendar for start date 
+            scope.openStartCalendar = function(){
                 event.preventDefault();
                 event.stopPropagation();
                 scope.opened = true;
-                scope.$apply();
-            });
-            element.find('.buttonend').bind('click', function (event) {
+            };
+
+            // Open the second calendar for end date 
+            scope.openEndCalendar = function(){
                 event.preventDefault();
                 event.stopPropagation();
                 scope.opened2 = true;
-                scope.$apply();
-            });
+            };
+
+            // Basic input control: limit min date on the end date. 
+            scope.$watch('startdate', function(newDate, oldDate, scope) {
+                if(newDate){
+                    scope.minEndDate = scope.startdate;
+                    if(scope.enddate < scope.startdate){
+                        // User needs to reselect a new end date
+                        scope.enddate = null; 
+                    }
+                }
+            }, true);
         }
     };
 });
