16ee6eec12c47a972e3bd973cb67c9ac05400a9e
Santina Lin <santina424@gmail.com>
2015-08-19 23:38:16 -0400

    UI update with beacon search, date picker, chr
    - Initialize default dates for date time picker
    - Added an autocomplete beacon search chip bar
    - Hard coded chromosome keys to ensure order

--- src/js/controllers/admins_genome.js
+++ src/js/controllers/admins_genome.js
@@ -5,10 +5,12 @@ angular.module('RDash').controller('genomeCtrl', ["$scope", "chrBands", "mockedA
 
    	// Get the data 
     var chromosomeData = _.countBy(chrBands.hg18, 'chr');
-    var chromosomeArray =  _.keys(chromosomeData);
     
-    // Get a list of chromosomes 
-    $scope.chromosomes = chromosomeArray; 
+    // a list of chromosomes 
+    $scope.chromosomes = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", 
+        "chr7", "chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", 
+        "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22", 
+        "chrX", "chrY"] 
 
     // Initialize for the first chromosome option 
     $scope.chr = $scope.chromosomes[0];
@@ -43,7 +45,54 @@ angular.module('RDash').controller('genomeCtrl', ["$scope", "chrBands", "mockedA
     	}
     }
 
+    /* Date and time range */
 
+    // Set default to one month period 
+    $scope.today = new Date();
+    var lastMonth = new Date();
+    lastMonth.setMonth($scope.today.getMonth() - 1); 
 
- 
+    $scope.BHdate2 = new Date($scope.today); $scope.BHdate1 = new Date(lastMonth);  
+
+    /* Beacon chip search bar */ 
+    $scope.searchBarText = "Loading..."
+    $scope.readonly = false;
+    $scope.selectedItem = null;
+    $scope.searchText = null;
+    $scope.querySearch = querySearch;
+    $scope.beacons = [];
+    $scope.selectedBeacons = [];
+    $scope.numberChips = [];
+    $scope.numberChips2 = [];
+    $scope.numberBuffer = '';
+
+    // Search for beacon
+    function querySearch (query) {
+      var results = query ? $scope.beacons.filter(createFilterFor(query)) : [];
+      return results;
+    }
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(beacon) {
+        return (beacon._lowername.indexOf(lowercaseQuery) === 0) ||
+            (beacon._lowertype.indexOf(lowercaseQuery) === 0);
+      };
+    }
+
+    mockedAPI.getBeacons().success(function(response){
+        var beaconInfo = response; 
+        for(i in beaconInfo){
+            beaconInfo[i]._lowername = beaconInfo[i].id.toLowerCase();
+            beaconInfo[i]._lowertype = beaconInfo[i].organization.id.toLowerCase();
+        }
+        $scope.beacons = beaconInfo;
+
+        // Update search bar placeholder text 
+        $scope.searchBarText = "Search for beacons"; 
+
+    });
+
 }]); 
